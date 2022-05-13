227f13d900348ecf51159c38346299aafa7ece82
Leonid Inozemtsev <92368620+Kintobor1370@users.noreply.github.com>
2022-05-13 04:04:07 +0300

    Add files via upload

--- Dll1/SplineLibrary.h
+++ Dll1/SplineLibrary.h
@@ -1,4 +1,4 @@
 #pragma once
 
 extern "C"  _declspec(dllexport)
-void SplineBuild(int nx, double* Scope, double* NodeArray, double* ValueArray, double* Der, double* Result);
+void SplineBuild(int nx, int nsites, double* Scope, double* NodeArray, double* ValueArray, double* Der, double* Result);
